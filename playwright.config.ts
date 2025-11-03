34e71798f218264eff701309038d9ea2f1d2790a
EngineerLoureiro <3p1cbano94@gmail.com>
2025-11-03 19:36:58 +0000

    Add configuration to support usage of dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,8 +6,6 @@ import dotenv from "dotenv";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 /**
